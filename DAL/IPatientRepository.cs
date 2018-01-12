b9283b224b1a6baa74bcabe492b36a6d219f1256
DESKTOP-LBHS619\Buddhima <kgbuddhima@gmail.com>
2018-01-12 21:33:36 +0530

    DAL : patient Repository -> Basic functions added

--- DAL/IPatientRepository.cs
+++ DAL/IPatientRepository.cs
@@ -9,6 +9,37 @@ namespace DAL
 {
     public interface IPatientRepository
     {
+        /// <summary>
+        /// Get patient by ID
+        /// </summary>
+        /// <param name="patientId"></param>
+        /// <returns></returns>
+        Patient GetPatient(int patientId);
+
+        /// <summary>
+        /// Get all patients
+        /// </summary>
+        /// <returns></returns>
+        List<Patient> GetPatientCollection();
+
+        /// <summary>
+        /// Get total number of patients
+        /// </summary>
+        /// <returns></returns>
+        int? GetPatientCount();
+
+        /// <summary>
+        /// Save Patient
+        /// </summary>
+        /// <param name="patient"></param>
+        /// <returns></returns>
         bool SavePatient(Patient patient);
+
+        /// <summary>
+        /// Update Patient
+        /// </summary>
+        /// <param name="patient"></param>
+        /// <returns></returns>
+        bool UpdatePatient(Patient patient);
     }
 }
--- DAL/PatientRepository.cs
+++ DAL/PatientRepository.cs
@@ -4,14 +4,126 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BusinessEntity;
+using Dapper;
+using System.Data.Sql;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace DAL
 {
     public class PatientRepository : IPatientRepository
     {
+        IDbConnection conn { get; set; }
+
+        public IDbConnection dbConnection
+        {
+            get
+            {
+                if (conn == null)
+                    return new SqlConnection(ConfigurationManager.AppSettings.Get("EHR_DB_CON"));
+                return conn;
+            }
+        }
+
+        /// <summary>
+        /// Get patient by ID
+        /// </summary>
+        /// <param name="patientId"></param>
+        /// <returns></returns>
+        public Patient GetPatient(int patientId)
+        {
+            try
+            {
+                using (IDbConnection cn = dbConnection)
+                {
+                    return cn.Query<Patient>("GetPatientById", new { PatientId= patientId }, commandType: CommandType.StoredProcedure).FirstOrDefault();
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Get all patients
+        /// </summary>
+        /// <returns></returns>
+        public List<Patient> GetPatientCollection()
+        {
+            try
+            {
+                using (IDbConnection cn = dbConnection)
+                {
+                    return cn.Query<Patient>("GetPatientCollection", commandType: CommandType.StoredProcedure).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Get total number of patients
+        /// </summary>
+        /// <returns></returns>
+        public int? GetPatientCount()
+        {
+            try
+            {
+                using (IDbConnection cn = dbConnection)
+                {
+                    return cn.Query<int>("GetPatientCount", commandType: CommandType.StoredProcedure).FirstOrDefault();
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Save Patient
+        /// </summary>
+        /// <param name="patient"></param>
+        /// <returns></returns>
         public bool SavePatient(Patient patient)
         {
-            return true;
+            try
+            {
+                using (IDbConnection cn = dbConnection)
+                {
+                    int result = cn.Execute("SavePatient", new { }, commandType: CommandType.StoredProcedure);
+                    return Convert.ToBoolean(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Update Patient
+        /// </summary>
+        /// <param name="patient"></param>
+        /// <returns></returns>
+        public bool UpdatePatient(Patient patient)
+        {
+            try
+            {
+                using (IDbConnection cn = dbConnection)
+                {
+                    int result = cn.Execute("UpdatePatient", new { }, commandType: CommandType.StoredProcedure);
+                    return Convert.ToBoolean(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
         }
     }
 }
