b3d343322914eb6199e51316bfc62151e0e67665
DESKTOP-LBHS619\Buddhima <kgbuddhima@gmail.com>
2018-01-13 16:10:49 +0530

    BLL : Methods added to interface and document

--- BLL/IPatientDocument.cs
+++ BLL/IPatientDocument.cs
@@ -9,8 +9,50 @@ namespace BLL
 {
     public interface IPatientDocument
     {
+        /// <summary>
+        /// Get patient by ID
+        /// </summary>
+        /// <returns></returns>
+        int? GetNextPatientId();
+
+        /// <summary>
+        /// Get patient by ID
+        /// </summary>
+        /// <param name="patientId"></param>
+        /// <returns></returns>
         Patient GetPatient(int patientId);
 
-        bool SavePatient(string patient);
+        /// <summary>
+        /// Get address of a patient
+        /// </summary>
+        /// <param name="patientId"></param>
+        /// <returns></returns>
+        Address GetPatientAddress(int patientId);
+
+        /// <summary>
+        /// Get all patients
+        /// </summary>
+        /// <returns></returns>
+        List<Patient> GetPatientCollection();
+
+        /// <summary>
+        /// Get total number of patients
+        /// </summary>
+        /// <returns></returns>
+        int? GetPatientCount();
+
+        /// <summary>
+        /// Save Patient
+        /// </summary>
+        /// <param name="patient"></param>
+        /// <returns></returns>
+        bool SavePatient(Patient patient);
+
+        /// <summary>
+        /// Update Patient
+        /// </summary>
+        /// <param name="patient"></param>
+        /// <returns></returns>
+        bool UpdatePatient(Patient patient);
     }
 }
--- BLL/PatientDocument.cs
+++ BLL/PatientDocument.cs
@@ -13,26 +13,146 @@ namespace BLL
     {
         IPatientRepository _repository;
 
-        public PatientDocument()//IPatientRepository repository
+        public PatientDocument()
         {
             _repository = new PatientRepository();
         }
 
-        public Patient GetPatient(int patientId)
+        /// <summary>
+        /// Get patient by ID
+        /// </summary>
+        /// <returns></returns>
+        public int? GetNextPatientId()
         {
-            Patient p = new Patient()
+            try
             {
-                ID = 1,
-                Name = "buddhima",
-                NIC = "12233"
-            };
-            return p;
+                return _repository.GetNextPatientId();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
-        public bool SavePatient(string patient)
+        /// <summary>
+        /// Get address of a patient
+        /// </summary>
+        /// <param name="patientId"></param>
+        /// <returns></returns>
+        public Address GetPatientAddress(int patientId)
         {
-            Patient p = JsonConvert.DeserializeObject<Patient>(patient);
-            return _repository.SavePatient(p);
+            try
+            {
+                if (patientId > 0)
+                {
+                    return _repository.GetPatientAddress(patientId);
+                }
+                else return null;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Get patient by ID
+        /// </summary>
+        /// <param name="patientId"></param>
+        /// <returns></returns>
+        public Patient GetPatient(int patientId)
+        {
+            try
+            {
+                if (patientId > 0)
+                {
+                    Patient result = _repository.GetPatient(patientId);
+                    if (result != null)
+                    {
+                        result.Address = GetPatientAddress(patientId);
+                    }
+                    return result;
+                }
+                else throw new NullReferenceException();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Get all patients
+        /// </summary>
+        /// <returns></returns>
+        public List<Patient> GetPatientCollection()
+        {
+            try
+            {
+                return _repository.GetPatientCollection();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Get total number of patients
+        /// </summary>
+        /// <returns></returns>
+        public int? GetPatientCount()
+        {
+            try
+            {
+                return _repository.GetPatientCount();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Save Patient
+        /// </summary>
+        /// <param name="patient"></param>
+        /// <returns></returns>
+        public bool SavePatient(Patient patient)
+        {
+            try
+            {
+                if (patient != null)
+                {
+                    return _repository.SavePatient(patient);
+                }
+                else throw new NullReferenceException();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Update Patient
+        /// </summary>
+        /// <param name="patient"></param>
+        /// <returns></returns>
+        public bool UpdatePatient(Patient patient)
+        {
+            try
+            {
+                if (patient != null)
+                {
+                    return _repository.UpdatePatient(patient);
+                }
+                else throw new NullReferenceException();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }                    
         }
     }
 }
