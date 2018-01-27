cf66f7c2b8dede4825b9d50973c7e018883d97a1
DESKTOP-LBHS619\Buddhima <kgbuddhima@gmail.com>
2018-01-27 23:08:11 +0530

    PL: LoginController added / Patient controller updated

---
+++ EHRDAService/Controllers/LoginController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BLL;
+using BusinessEntity;
+using Utility;
+
+namespace EHRDAService.Controllers
+{
+    [RoutePrefix("api/Login")]
+    public class LoginController : ApiController
+    {
+        IPatientDocument _document;
+
+        public LoginController()
+        {
+            _document = new PatientDocument();
+        }
+
+        [Route("ChekPatientLogin")]
+        [HttpPost]
+        public HttpResponseMessage CheckPatientLogin(Credentials credentials)
+        {
+            try
+            {
+                int patientId = _document.CheckPatientLogin(credentials);
+                if (patientId > 0)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, patientId);
+                }
+                else
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, CommonUnit.oFailed);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        [Route("CheckStaffLogin")]
+        [HttpPost]
+        public HttpResponseMessage CheckStaffLogin(Credentials credentials)
+        {
+            try
+            {
+                int staffId = _document.CheckStaffLogin(credentials);
+                if (staffId > 0)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, staffId);
+                }
+                else
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, CommonUnit.oFailed);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+    }
+}
--- EHRDAService/Controllers/PatientController.cs
+++ EHRDAService/Controllers/PatientController.cs
@@ -20,46 +20,6 @@ namespace EHRDAService.Controllers
             _document = new PatientDocument();
         }
 
-        [Route("ChekPatientLogin")]
-        [HttpPost]
-        public HttpResponseMessage CheckPatientLogin(Credentials credentials)
-        {
-            try
-            {
-                int patientId = _document.CheckPatientLogin(credentials);
-                if (patientId > 0)
-                {
-                    return Request.CreateResponse(HttpStatusCode.OK, patientId);
-                }
-                else
-                    return Request.CreateResponse(HttpStatusCode.BadRequest, CommonUnit.oFailed);
-            }
-            catch (Exception ex)
-            {
-                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
-            }
-        }
-
-        [Route("CheckStaffLogin")]
-        [HttpPost]
-        public HttpResponseMessage CheckStaffLogin(Credentials credentials)
-        {
-            try
-            {
-                int staffId = _document.CheckStaffLogin(credentials);
-                if (staffId > 0)
-                {
-                    return Request.CreateResponse(HttpStatusCode.OK, staffId);
-                }
-                else
-                    return Request.CreateResponse(HttpStatusCode.BadRequest, CommonUnit.oFailed);
-            }
-            catch (Exception ex)
-            {
-                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
-            }
-        }
-
         [Route("DeletePatient")]
         [HttpPost]
         public HttpResponseMessage DeletePatient(int id)
